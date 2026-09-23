9dcf5b354aae557e84cb0c5499b9b3f39f26c8d8
GenTwin123 <GenTwin123@users.noreply.github.com>
2026-09-23 11:18:14 +0800

    Set result comparison videos to 0.8x

--- static/js/index.js
+++ static/js/index.js
@@ -329,8 +329,8 @@ function createComparisonCard(method, sceneId) {
     card.appendChild(header);
 
     var video = createVideoElement(resultVideoPath(method, sceneId));
-    video.defaultPlaybackRate = 0.9;
-    video.playbackRate = 0.9;
+    video.defaultPlaybackRate = 0.8;
+    video.playbackRate = 0.8;
     video.addEventListener("error", function () { card.classList.add("video-unavailable"); });
     card.appendChild(video);
     return card;
